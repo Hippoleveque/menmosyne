eb99cdf6ed170584344999c5eac69628d4dec277
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2023-01-27 21:27:08 +0100

    Changed the route for revision of a specific collection.

--- app/src/App.js
+++ app/src/App.js
@@ -14,7 +14,7 @@ function App() {
       {isLoggedIn && (
         <Fragment>
           <Route path="/" exact element={<HomePage />} />
-          <Route path="/revision" exact element={<RevisionPage />} />
+          <Route path="/revision/:collectionId" element={<RevisionPage />} />
         </Fragment>
       )}
       {!isLoggedIn && (
