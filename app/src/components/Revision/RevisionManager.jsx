8b3be64253635eff7fdfeebf2bfd85ad332a25ab
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2023-01-29 16:18:57 +0100

    Refactored the Revision components and redirect to the home page at the end of a revision.

---
+++ app/src/components/Revision/RevisionManager.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../store/auth-context";
+import RevisionCard from "./RevisionCard";
+
+export default function RevisionManager({ collectionId }) {
+  const { loginToken } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [cards, setCards] = useState([]);
+  const [currentCardIndex, setCurrentCardIndex] = useState(null);
+
+  useEffect(() => {
+    const fetchCards = async () => {
+      const response = await fetch(`/api/memo/cards/${collectionId}`, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + loginToken,
+        },
+      });
+      const res = await response.json();
+      setCards(res.cards);
+      if (res.cards.length > 0) {
+        setCurrentCardIndex(0);
+      }
+    };
+    fetchCards();
+  }, [loginToken, collectionId]);
+
+  useEffect(() => {
+    if (currentCardIndex && currentCardIndex >= cards.length) {
+      navigate("/");
+    }
+    console.log(
+      "Use Effect for navigation has been called",
+      currentCardIndex,
+      cards.length
+    );
+  }, [currentCardIndex, cards, navigate]);
+
+  const handleActionClick = (e) => {
+    setCurrentCardIndex((currVal) => currVal + 1);
+  };
+
+  return currentCardIndex < cards.length ? (
+    <RevisionCard
+      card={cards[currentCardIndex]}
+      handleActionClick={handleActionClick}
+    />
+  ) : (
+    <p>Il n'y a pas de cartes dans cette collection</p>
+  );
+}
--- app/src/pages/RevisionPage.jsx
+++ app/src/pages/RevisionPage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 import { useParams } from "react-router-dom";
-import RevisionContent from "../components/Revision/RevisionContent";
+import RevisionManager from "../components/Revision/RevisionManager";
 import SideMenuLayout from "../components/Layouts/SideMenuLayout";
 
 export default function RevisionPage() {
   const { collectionId } = useParams();
   return (
     <SideMenuLayout>
-      <RevisionContent collectionId={collectionId} />
+      <RevisionManager collectionId={collectionId} />
     </SideMenuLayout>
   );
 }
