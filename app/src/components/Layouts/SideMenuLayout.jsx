226b3083a2381e310dad339fd2c0056c6efba309
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2023-01-31 09:00:01 +0100

    Added an avatar on the home page.

--- app/src/components/Layouts/SideMenuLayout.jsx
+++ app/src/components/Layouts/SideMenuLayout.jsx
@@ -3,6 +3,7 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemButton from "@mui/material/ListItemButton";
+import Avatar from "@mui/material/Avatar";
 
 import classes from "./SideMenuLayout.module.css";
 
@@ -10,7 +11,8 @@ export default function SideMenuLayout(props) {
   return (
     <Grid container className={classes.gridContainer} component="main">
       <Grid item xs={6} md={2} className={classes.grid}>
-        <List>
+        <List className={classes.sideMenuItems}>
+          <Avatar>H</Avatar>
           {["Home"].map((text, index) => (
             <ListItem key={text} disablePadding>
               <ListItemButton>
