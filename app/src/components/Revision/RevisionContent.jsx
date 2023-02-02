8bef76698ed8da44e472761d3112f646eaf62e25
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2023-02-02 21:07:00 +0100

    Removed useless commented code and fixed the bug where the currentOffset was not computed.

--- app/src/components/Revision/RevisionContent.jsx
+++ app/src/components/Revision/RevisionContent.jsx
@@ -9,26 +9,8 @@ export default function RevisionContent({ collectionId }) {
   const { loginToken } = useContext(AuthContext);
   const navigate = useNavigate();
   const [cards, setCards] = useState([]);
-  const [numCards, setNumCards] = useState(null);
   const [currentOffset, setCurrentOffset] = useState(0);
   const [currentCardIndex, setCurrentCardIndex] = useState(null);
-
-  // const fetchCards = useCallback(async () => {
-  //   const response = await fetch(`/api/memo/cards/${collectionId}`, {
-  //     method: "GET",
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //       Authorization: "Bearer " + loginToken,
-  //     },
-  //   });
-  //   const res = await response.json();
-  //   setCards(res.cards);
-  //   setNumCards(res.totalCards);
-  //   if (res.cards.length > 0) {
-  //     setCurrentCardIndex(0);
-  //   }
-  // }, [loginToken, collectionId]);
 
   useEffect(() => {
     const fetchCards = async () => {
@@ -45,8 +27,8 @@ export default function RevisionContent({ collectionId }) {
       );
       const res = await response.json();
       setCards(res.cards);
-      setNumCards(res.totalCards);
       setCurrentCardIndex(0);
+      setCurrentOffset((currOffset) => currOffset + ITEMS_IN_MEMORY);
     };
     fetchCards();
   }, [loginToken, collectionId, currentOffset]);
