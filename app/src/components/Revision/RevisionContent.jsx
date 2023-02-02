da7f220f2f8ab8f5b3de956e4159fa567326ca25
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2023-02-02 21:04:13 +0100

    Refactored the RevisionContent file to take into account the pagination mecanism of the backend.

--- app/src/components/Revision/RevisionContent.jsx
+++ app/src/components/Revision/RevisionContent.jsx
@@ -1,32 +1,55 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../store/auth-context";
 import RevisionCard from "./RevisionCard/RevisionCard";
+
+const ITEMS_IN_MEMORY = 10;
 
 export default function RevisionContent({ collectionId }) {
   const { loginToken } = useContext(AuthContext);
   const navigate = useNavigate();
   const [cards, setCards] = useState([]);
+  const [numCards, setNumCards] = useState(null);
+  const [currentOffset, setCurrentOffset] = useState(0);
   const [currentCardIndex, setCurrentCardIndex] = useState(null);
+
+  // const fetchCards = useCallback(async () => {
+  //   const response = await fetch(`/api/memo/cards/${collectionId}`, {
+  //     method: "GET",
+  //     headers: {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //       Authorization: "Bearer " + loginToken,
+  //     },
+  //   });
+  //   const res = await response.json();
+  //   setCards(res.cards);
+  //   setNumCards(res.totalCards);
+  //   if (res.cards.length > 0) {
+  //     setCurrentCardIndex(0);
+  //   }
+  // }, [loginToken, collectionId]);
 
   useEffect(() => {
     const fetchCards = async () => {
-      const response = await fetch(`/api/memo/cards/${collectionId}`, {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + loginToken,
-        },
-      });
+      const response = await fetch(
+        `/api/memo/cards/${collectionId}?offset=${currentOffset}&limit=${ITEMS_IN_MEMORY}`,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + loginToken,
+          },
+        }
+      );
       const res = await response.json();
       setCards(res.cards);
-      if (res.cards.length > 0) {
-        setCurrentCardIndex(0);
-      }
+      setNumCards(res.totalCards);
+      setCurrentCardIndex(0);
     };
     fetchCards();
-  }, [loginToken, collectionId]);
+  }, [loginToken, collectionId, currentOffset]);
 
   useEffect(() => {
     if (currentCardIndex && currentCardIndex >= cards.length) {
