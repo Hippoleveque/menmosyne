0de4dac6179e89705f075f3cba8e01c693047994
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2023-02-07 22:12:44 +0100

    Replaced the old side menu with a drawer component.

--- app/src/components/Layouts/SideMenuLayout.jsx
+++ app/src/components/Layouts/SideMenuLayout.jsx
@@ -7,9 +7,13 @@ import ListItemButton from "@mui/material/ListItemButton";
 import Avatar from "@mui/material/Avatar";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import Drawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
 
 import { AuthContext } from "../../store/auth-context";
 import classes from "./SideMenuLayout.module.css";
+
+const drawerWidth = 240;
 
 export default function SideMenuLayout(props) {
   const { onLogout } = useContext(AuthContext);
@@ -32,33 +36,66 @@ export default function SideMenuLayout(props) {
   };
 
   return (
-    <Grid container className={classes.gridContainer} component="main">
-      <Grid item xs={6} md={2} className={classes.grid}>
-        <List className={classes.sideMenuItems}>
-          <Avatar onClick={handleClick}>H</Avatar>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-          >
-            <MenuItem onClick={() => handleClose("logout")}>Logout</MenuItem>
-          </Menu>
-          {["Home"].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
-                <ListItemText primary={text} className={classes.sideMenuItem} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Grid>
-      <Grid item xs={6} md={10}>
+    <Box sx={{ display: "flex" }}>
+      <Box
+        component="nav"
+        sx={{
+          width: { sm: drawerWidth },
+          flexShrink: { sm: 0 },
+        }}
+        className={classes.sideMenu}
+      >
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+            color: "red",
+          }}
+          PaperProps={{sx : {backgroundColor: "#042141"}}}
+          open
+        >
+          <List className={classes.sideMenuItems}>
+            <Avatar sx={{ textAlign: "center" }} onClick={handleClick}>
+              H
+            </Avatar>
+            <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
+              }}
+            >
+              <MenuItem onClick={() => handleClose("logout")}>Logout</MenuItem>
+            </Menu>
+            {["Home"].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemText
+                    primary={text}
+                    className={classes.sideMenuItem}
+                  />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
         {props.children}
-      </Grid>
-    </Grid>
+      </Box>
+    </Box>
   );
 }
