e628365cad4d32e3fcd2ed1672ecec00a47746dd
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2023-02-10 22:04:53 +0100

    Cosmetics fixes on the side menu panel & navigation is working.

--- app/src/components/Layouts/SideMenuLayout.jsx
+++ app/src/components/Layouts/SideMenuLayout.jsx
@@ -1,14 +1,19 @@
 import React, { useState, useContext } from "react";
-import Grid from "@mui/material/Grid";
+import { useNavigate } from "react-router-dom";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
 import Avatar from "@mui/material/Avatar";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Drawer from "@mui/material/Drawer";
 import Box from "@mui/material/Box";
+import HomeIcon from "@mui/icons-material/Home";
+import Divider from "@mui/material/Divider";
+import { SvgIcon } from "@mui/material";
+import Logo from "../Common/MnemosyneLogo";
 
 import { AuthContext } from "../../store/auth-context";
 import classes from "./SideMenuLayout.module.css";
@@ -18,6 +23,7 @@ const drawerWidth = 240;
 export default function SideMenuLayout(props) {
   const { onLogout } = useContext(AuthContext);
   const [anchorEl, setAnchorEl] = useState(null);
+  const navigate = useNavigate();
   const open = Boolean(anchorEl);
 
   const handleClick = (event) => {
@@ -29,6 +35,16 @@ export default function SideMenuLayout(props) {
     switch (selectedAction) {
       case "logout":
         onLogout();
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleNavMenuClick = (selectedMenu) => {
+    switch (selectedMenu) {
+      case "Home":
+        navigate("/");
         break;
       default:
         break;
@@ -55,13 +71,27 @@ export default function SideMenuLayout(props) {
             },
             color: "red",
           }}
-          PaperProps={{sx : {backgroundColor: "#042141"}}}
+          PaperProps={{ sx: { backgroundColor: "#042141" } }}
           open
         >
           <List className={classes.sideMenuItems}>
-            <Avatar sx={{ textAlign: "center" }} onClick={handleClick}>
-              H
-            </Avatar>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-around",
+                width: "100%",
+              }}
+            >
+              <SvgIcon
+                component={Logo}
+                viewBox="0 0 75 75"
+                sx={{ fontSize: "2.5rem" }}
+              />
+              <Avatar sx={{ textAlign: "center" }} onClick={handleClick}>
+                H
+              </Avatar>
+            </Box>
             <Menu
               id="basic-menu"
               anchorEl={anchorEl}
@@ -73,9 +103,15 @@ export default function SideMenuLayout(props) {
             >
               <MenuItem onClick={() => handleClose("logout")}>Logout</MenuItem>
             </Menu>
+            <Divider
+              sx={{ bgcolor: "white", width: "100%", marginTop: "1rem" }}
+            />
             {["Home"].map((text, index) => (
-              <ListItem key={text} disablePadding>
-                <ListItemButton>
+              <ListItem key={text}>
+                <ListItemButton onClick={() => handleNavMenuClick(text)}>
+                  <ListItemIcon>
+                    <HomeIcon sx={{ color: "white", size: 0.5 }} />
+                  </ListItemIcon>
                   <ListItemText
                     primary={text}
                     className={classes.sideMenuItem}
