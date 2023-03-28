90dc0fb212d60ebb92e2c4fb3cc22562d2690c4c
Hippolyte Lévêque <hippolyte.leveque@student.ecp.fr>
2023-03-28 19:26:12 +0200

    Added the edit collection endpoint

--- api/routes/collections.js
+++ api/routes/collections.js
@@ -8,8 +8,13 @@ import {
   deleteCollection,
   getCollectionCards,
   getCollectionCardsToReview,
+  editCollection,
 } from "../controllers/collections.js";
 import { isAuth } from "../middlewares/isAuth.js";
+
+const checkCorrectPolicy = (value) => {
+  return typeof value === "number" && value >= 0;
+};
 
 const router = express.Router();
 
@@ -26,6 +31,16 @@ router.post(
   [body("name").trim().isLength({ min: 3 })],
   isAuth,
   createCollection
+);
+router.post(
+  "/:collectionId",
+  [
+    body("name").trim().isLength({ min: 3 }),
+    body("newCardsPolicy").custom(checkCorrectPolicy),
+    body("reviewCardsPolicy").custom(checkCorrectPolicy),
+  ],
+  isAuth,
+  editCollection
 );
 router.delete("/:collectionId", isAuth, deleteCollection);
 
